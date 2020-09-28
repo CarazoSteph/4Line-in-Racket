b97d53812f2efeeffa8af873e4f4263dba05d185
yenus210 <yen21000@gmail.com>
2020-09-28 12:34:44 -0600

    Se añade la función viabilidad y objetivo

--- Greedy/greedy.rkt
+++ Greedy/greedy.rkt
@@ -113,26 +113,104 @@
     (append (list matriz)  (append (list(matdiag matriz)) (list(matdiag (falsaTraspuesta matriz)))))
 )
 
-;;Función auxiliar de seleccion, busca posibles lineas donde colocar fichas, estu funcion busca pares de fichas
+;;Función auxiliar de seleccion, busca posibles lineas donde colocar fichas, estu funcion busca pares de fichas fichas en 101
 (define (linea2? lista)
     (cond ((null? (cdr lista))
         #f
-    )((or (= (car lista) 1) (= (car lista) 2))
-        (cond((= (car lista) (cadr lista))
+    )
+    ((null? (cddr lista))
+        #f
+    )    
+    ((or (= (car lista) 1) (= (car lista) 2))
+        (cond
+        ((or (= (car lista) (cadr lista)) (and (= (car lista) (caddr lista)) (= (cadr lista) 0)))
             #t
         )(else
         (linea2? (cdr lista))
-        ))
-    )(else
+        )))
+    ((or (= (car (reverse lista)) 1) (= (car (reverse lista)) 2))
+        (cond
+        ((or (= (car (reverse lista)) (cadr (reverse lista))) (and (= (car (reverse lista)) (caddr (reverse lista))) (= (cadr (reverse lista)) 0)))
+            #t
+        )(else
+        (linea2? (cdr (reverse lista)))
+        )))
+    (else
         (linea2? (cdr lista))
         )
-)
-)
-;;
+))
+
+;;Selecciona posibles soluciones
 (define (seleccion candidatos)
-    (append (filter linea2? (car candidatos)) (filter linea2? (cadr candidatos)) (filter linea2? (caddr candidatos)))
+    (append (list(filter linea2? (car candidatos))) (list(filter linea2? (cadr candidatos))) (list(filter linea2? (caddr candidatos))))
+)
+;;De las posibles soluciones escoge cuales son viables para ser una solucion
+(define (viabilidadAux? lista)
+    (cond 
+    ((null?(cdddr lista))
+    #f
+    )
+    ((or (= (car lista) 1) (= (car lista) 2))
+            (cond((and (=(car lista) (caddr lista)) (= (cadr lista) 0) (=(cadddr lista) 0))
+                #t)
+            ((and (=(car lista) (cadr lista)) (= (caddr lista) 0) (=(cadddr lista) 0))
+                #t)
+            ((and (=(car lista) (cadr lista) (caddr lista)) (=(cadddr lista) 0))
+                #t)
+            (else
+        (viabilidadAux? (cdr lista))))
+        )
+    ((or (= (car (reverse lista)) 1) (= (car (reverse lista)) 2))
+        (cond((and (=(car (reverse lista)) (caddr (reverse lista))) (= (cadr (reverse lista)) 0) (=(cadddr (reverse lista)) 0))
+                #t)
+            ((and (=(car (reverse lista)) (cadr (reverse lista))) (= (caddr (reverse lista)) 0) (=(cadddr (reverse lista)) 0))
+                #t)
+            ((and (=(car (reverse lista)) (cadr (reverse lista)) (caddr (reverse lista))) (=(cadddr (reverse lista)) 0))
+                #t)
+            (else
+        (viabilidadAux? (cdr (reverse lista)))))
+        )
+
+    (else
+        (viabilidadAux? (cdr lista))))
+)
+;;De las posibles soluciones escoge cuales son viables para ser una solucion
+(define (viabilidad candidatos)
+    (filter viabilidadAux?  (car candidatos))
 )
 
+(define (objetivo lista)
+    (cond 
+    ((null?(cdddr lista))
+    #f
+    )
+    ((or (= (car lista) 1) (= (car lista) 2))
+            (cond((and (=(car lista) (caddr lista)) (= (cadr lista) 0) (=(cadddr lista) 0))
+                #t)
+            ((and (=(car lista) (cadr lista)) (= (caddr lista) 0) (=(cadddr lista) 0))
+                #t)
+            ((and (=(car lista) (cadr lista) (caddr lista)) (=(cadddr lista) 0))
+                #t)
+            (else
+        (viabilidadAux? (cdr lista))))
+        )
+    ((or (= (car (reverse lista)) 1) (= (car (reverse lista)) 2))
+        (cond((and (=(car (reverse lista)) (caddr (reverse lista))) (= (cadr (reverse lista)) 0) (=(cadddr (reverse lista)) 0))
+                #t)
+            ((and (=(car (reverse lista)) (cadr (reverse lista))) (= (caddr (reverse lista)) 0) (=(cadddr (reverse lista)) 0))
+                #t)
+            ((and (=(car (reverse lista)) (cadr (reverse lista)) (caddr (reverse lista))) (=(cadddr (reverse lista)) 0))
+                #t)
+            (else
+        (viabilidadAux? (cdr (reverse lista)))))
+        )
+
+    (else
+        (viabilidadAux? (cdr lista))))
+
+)
+
+;;((2 0 2 0 1 2 0 0) (0 0 0 1 1) (2 2 2))
 
 (seleccion (candidatos '(
     (0 0 0 0 0 0 0 0) 
@@ -141,10 +219,6 @@
     (0 0 0 0 0 0 0 0) 
     (0 0 0 0 0 0 0 0)
     (0 0 0 0 0 0 0 0)
-    (0 0 0 1 0 0 2 0)
-    (2 2 2 2 1 2 0 0))))
-;;(matdiag '((1 2 3 4 5) (2 3 4 5 6) (3 4 5 6 7) (4 5 6 7 8) (5 6 7 8 9)))
-;;(matdiag  '((1 2 3) (3 4 5) (5 6 7) (7 8 9)))
-;;(matdiag '((1 2 3 4) (2 3 4 5) (3 4 5 7)))
-;;(falsaTraspuesta '((1 2 3 4) (2 3 4 5) (3 4 5 7)))
-;;(matdiag (falsaTraspuesta '((1 2 3 4) (2 3 4 5) (3 4 5 7))))
+    (1 0 1 0 1 0 0 0)
+    (2 0 2 0 2 1 2 1))))
+
